4d4e2758604cde3adefba1330c99df0e748c9964
DhwaniBhavankarKarthikeyan <116158619+DhwaniBhavankarKarthikeyan@users.noreply.github.com>
2023-10-25 10:04:23 +0530

    Update app.py

--- app.py
+++ app.py
@@ -10,6 +10,7 @@ from sklearn.neighbors import KNeighborsClassifier
 from sklearn.metrics import accuracy_score
 from sklearn.preprocessing import OneHotEncoder
 from sklearn.impute import SimpleImputer
+from sklearn.preprocessing import LabelEncoder
 
 # Load your dataset
 df = pd.read_csv("Finaldf-2.csv")

07d92f9fd696ab41aaaab38161f5eb34239c6559
DhwaniBhavankarKarthikeyan <116158619+DhwaniBhavankarKarthikeyan@users.noreply.github.com>
2023-10-25 10:15:04 +0530

    Update app.py

--- app.py
+++ app.py
@@ -5,7 +5,7 @@ import pandas as pd
 from sklearn.model_selection import train_test_split
 from sklearn.neighbors import KNeighborsClassifier 
 #from sklearn.linear_model import LogisticRegression
-#from sklearn.naive_bayes import GaussianNB
+from sklearn.naive_bayes import GaussianNB 
 #from sklearn.ensemble import RandomForestClassifier
 from sklearn.metrics import accuracy_score
 from sklearn.preprocessing import OneHotEncoder
